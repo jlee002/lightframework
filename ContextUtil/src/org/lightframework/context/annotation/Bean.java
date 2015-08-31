6e5f7ea4f1ade50ce64d05c62bfd069382cb034b
Jeffrey Lee <jlee002@gmail.com>
2015-08-30 23:48:46 -0400

    Added @Before and @After on TestUtil

--- ContextUtil/src/org/lightframework/context/annotation/Bean.java
+++ ContextUtil/src/org/lightframework/context/annotation/Bean.java
@@ -1,5 +1,9 @@
 package org.lightframework.context.annotation;
 
+/** Bean annotation for compatability
+ * @author Jeonghoon Lee
+ *
+ */
 public @interface Bean {
 
 }
--- ContextUtil/src/org/lightframework/context/annotation/Configuration.java
+++ ContextUtil/src/org/lightframework/context/annotation/Configuration.java
@@ -1,5 +1,10 @@
 package org.lightframework.context.annotation;
 
+/** Configuration annotation for compatability
+ * 
+ * @author Jeonghoon Lee
+ * 
+ */
 public @interface Configuration {
 
 }
